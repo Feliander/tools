d297ea0235ab0252ca3b0e091d27fc2a74d116ee
qazy777 <qazy777@gmail.com>
2026-07-19 00:13:30 +0300

    new tools

--- postgres/deb_init.sh
+++ postgres/deb_init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker-compose up -d;
+docker compose up -d;
 
 chmod -R 770 pgadmin_data;  # allow group write and disallow anything by others
 echo 'chmod pgadmin_data success';
 
 function check_postgres_is_ready() {  # checks if postgres is completely up
-  if docker-compose logs postgres | grep 'database system is ready to accept connections'; then
+  if docker compose logs postgres | grep 'database system is ready to accept connections'; then
     echo "postgres is ready";
     return "1";
   else
@@ -24,4 +24,4 @@ done
 chmod -R 770 pgdata; # allow rwx for group
 echo 'chmod pgdata success'
 
-docker-compose logs -f;
+docker compose logs -f;
